8a151a76e37aa43cdb9c641d010471d3bfecbd81
Chahat Gurung <chahatgurung12@gmail.com>
2026-06-11 09:58:10 +0530

    Add girls list and girl profile pages

---
+++ my-app/src/App.jsx
@@ -0,0 +1,18 @@
+//to view the girl's profile page remove the comments from the below written code and add treat the girls list function as comment
+
+/*import React from "react";
+import GirlProfile from "./pages/GirlProfile";
+
+function App() {
+  return <GirlProfile />;
+}*/
+
+
+import React from "react";
+import GirlsList from "./pages/GirlsList";
+
+function App() {
+  return <GirlsList />;
+}
+
+export default App;
